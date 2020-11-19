3fda9419d5a9347f63222fff7e1f36ec823940f6
Markus Poeschl <Poeschl@users.noreply.github.com>
2020-11-19 14:20:18 +0000

    First Run improvements

--- README.md
+++ README.md
@@ -14,3 +14,16 @@ Chromium will be inserted as autostart application.
 ## Configuration
 
 To configure the page the browser is starting change the `START_URL` parameter in the `run.sh` script in the `pi` user folder.
+
+## Screen rotation
+
+To change the screen rotation according to your setup edit the file `/boot/config.txt` and insert one of the below lines at the end.
+
+```bash
+display_rotate=0 # default rotation
+display_rotate=1 # 90° rotation
+display_rotate=2 # 180° rotation
+display_rotate=3 # 270° rotation
+```
+
+After a restart the orientation should be correct.
--- install.sh
+++ install.sh
@@ -4,37 +4,39 @@ set -e
 echo "Update apt"
 sudo apt-get update
 
-echo "Install unclutter to hide the mouse cursor..."
+echo "Install unclutter to hide the mouse cursor"
 sudo apt-get install unclutter -y --no-install-recommends
 
-echo "Adjust autostart (Screensaver, run script)..."
+echo "Adjust autostart (Screensaver, run script)"
 sudo sed -i.bak '/.*xscreensaver.*/d' /etc/xdg/lxsession/LXDE-pi/autostart
 printf "
 @xset s off
 @xset -dpms
 @xset s noblank
 
-@/home/pi/run.sh
+@/home/pi/start/fullscreen-browser.sh
 " | sudo tee -a /etc/xdg/lxsession/LXDE-pi/autostart
 
-echo "Update chromium..."
+echo "Update chromium"
 sudo apt-get install chromium-browser -y --no-install-recommends
 
 echo "Create run script"
-printf "#!/usr/bin/env sh
-set -x
+mkdir /home/pi/start
+printf '#!/usr/bin/env sh
+set -e
 
 START_URL="https://http.cat/200"
 
-/usr/bin/chromium-browser --app=$START_URL
-  --kiosk
-  --noerrdialogs
-  --disable-session-crashed-bubble
-  --disable-infobars
-  --check-for-update-interval=604800
+/usr/bin/chromium-browser \
+  --app=$START_URL \
+  --kiosk \
+  --noerrdialogs \
+  --disable-session-crashed-bubble \
+  --disable-infobars \
+  --check-for-update-interval=604800 \
   --disable-pinch
-" | sudo tee -a /home/pi/run.sh
-sudo chmod +x /home/pi/run.sh
+' > /home/pi/start/fullscreen-browser.sh
+sudo chmod +x /home/pi/start/fullscreen-browser.sh
 
 echo "Restart lightdm"
 sudo service lightdm restart
